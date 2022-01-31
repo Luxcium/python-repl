b8466200c8ab965f33b98ade42cf6d6b94af658a
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2022-01-31 13:24:48 -0500

    add/change LICENSE and NOTICE
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- NOTICE.sh
+++ NOTICE.sh
@@ -1,7 +1,7 @@
 ################################################################################
 ##                                                                            ##
 ##            !!! PLEASE USE CAUTION WHEN USING THESES FILES !!!              ##
-## THESES FILES *CANNOT* BE USED AS IS YOU MAY HAVE TO CUSTOMISE IT TO USE IT.##
+## THESES FILES *CANNOT* BE USED AS IS YOU MAY HAVE TO CUSTOMIZE IT TO USE IT.##
 ##  Even if this file is shared on my public git hub it has not been designed ##
 ##  for public use. It was not created with the idea that someone else would  ##
 ##  be using it. The files in this repository have been created for my usage  ##
@@ -12,37 +12,10 @@
 ##                                                                            ##
 ##                                                                            ##
 #+           Copyright (c) 2019-present Benjamin Vincent Kasapoglu            ##
-#&                                                                            ##
-#&     This Source Code Form is subject to the terms of the Mozilla Public    ##
-#&     License, v. 2.0. If a copy of the MPL was not distributed with this    ##
-#&           file, You can obtain one at http://mozilla.org/MPL/2.0/.         ##
-#&                                                                            ##
-##     The above copyright notice and this license notice shall be included   ##
-##           in all copies or substantial portions of the Software.           ##
-##                                                                            ##
-##                                                                            ##
-##          All distribution of Covered Software in Source Code Form,         ##
-##   including any Modifications that You create or to which You contribute,  ##
-##  must be under the terms of this License. You must inform recipients that  ##
-##  the Source Code Form of the Covered Software is governed by the terms of  ##
-##        this License, and how they can obtain a copy of this License.       ##
-##       You may not attempt to alter or restrict the recipients’rights       ##
-##                         in the Source Code Form.                           ##
 ##                                                                            ##
 ################################################################################
 
 ##!!0###########################################################################
-##!!                                                                          ##
-#+!!         Copyright (c) 2019-present Benjamin Vincent Kasapoglu            ##
-#&!!                                                                          ##
-#&!!   This Source Code Form is subject to the terms of the Mozilla Public    ##
-#&!!   License, v. 2.0. If a copy of the MPL was not distributed with this    ##
-#&!!         file, You can obtain one at http://mozilla.org/MPL/2.0/.         ##
-#&!!                                                                          ##
-##!!   The above copyright notice and this license notice shall be included   ##
-##!!         in all copies or substantial portions of the Software.           ##
-##!!                                                                          ##
-##!!          ------------------------------------------------------          ##
 ##!!                                                                          ##
 ##!!    Disclaimer of Warranty                                                ##
 ##!!   -------------------------                                              ##
